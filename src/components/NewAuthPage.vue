24c4a02d5a22b2ed6a06348d630b7ef566726ecd
Barry <ezeanibarry1@gmail.com>
2024-09-26 02:44:19 +0100

    Added the new auth page

---
+++ src/components/NewAuthPage.vue
@@ -0,0 +1,21 @@
+<script setup>
+</script>
+
+<template>
+  <div class="p-8">
+    <main class="flex justify-center">
+      <section>
+        <header>
+          <img src="/public/svgs/zipkod-authpage-logo.svg">
+        </header>
+        <form></form>
+      </section>
+    </main>
+    <footer>
+
+    </footer>
+  </div>
+</template>
+
+<style scoped>
+</style>
