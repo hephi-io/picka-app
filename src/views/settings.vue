0b7a4abda071e32cbc5449f65b1a143c4e5dcadb
Tochi-Ezeani <35009287+Tochi-Ezeani@users.noreply.github.com>
2024-12-13 17:52:15 +0100

    feat: settings header

--- src/views/settings.vue
+++ src/views/settings.vue
@@ -1,7 +1,28 @@
 <script setup></script>
 
 <template>
-  
+  <div>
+
+    <header class="py-3">
+      <span class="font-semibold text-2xl leading-6 tracking-[-0.03em] text-[#060E1F]">
+        Settings
+      </span>
+    </header>
+
+    <section class="mt-9"></section>
+
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+/* 
+font-family: Inter;
+font-size: 24px;
+font-weight: 600;
+line-height: 24px;
+letter-spacing: -0.03em;
+text-align: left;
+text-underline-position: from-font;
+text-decoration-skip-ink: none;
+*/
+</style>
